2eb522b45128a9220f3b2eced749c539b7b78b65
Jason <diurnalist@gmail.com>
2019-03-17 15:10:23 -0500

    Specify jekyll environment

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 export JEKYLL_VERSION=3.8
+export JEKYLL_ENVIRONMENT=development
 
 dir="${1:-_site}"
 docker run --rm \
   --volume="$PWD:/srv/jekyll" \
   --publish="4000:4000" \
   -it jekyll/minimal:$JEKYLL_VERSION \
-  jekyll serve --destination "$dir" "$@"
+  jekyll serve --destination "$dir" \
+  "$@"
